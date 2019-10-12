3f0f008383763e37ef3f0d7a1e7b418916c4b76c
Sudhir Kumar Ojha <sudhirkumarojhaa@gmail.com>
2019-10-12 15:22:48 +0530

    Added Comment to highlight the important steps.
    Comments are necessary for better understanding.

--- App.js
+++ App.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Linking } from 'react-native';
 import qs from 'qs';
 import config from './config.js';
+
+// OAuth function to handle the fitbit authentication URL.
+
 function OAuth(client_id, cb) {
  Linking.addEventListener('url', handleUrl);
  function handleUrl(event) {
-  console.log(event.url);
   Linking.removeEventListener('url', handleUrl);
   const [, query_string] = event.url.match(/\#(.*)/);
-  console.log(query_string);
   const query = qs.parse(query_string);
-  console.log(`query: ${JSON.stringify(query)}`);
   cb(query.access_token);
 }
+ 
 const oauthurl = `https://www.fitbit.com/oauth2/authorize?${qs.stringify({
  client_id,
  response_type: 'token',
  scope: 'heartrate activity activity profile sleep',
-redirect_uri:'fitbitTest1://fit',
+ redirect_uri:'fitbitTest1://fit', // it should be same as mentioned while registration at Fitbit.
  expires_in: '604800',
 })}`;
 console.log(oauthurl);
@@ -42,16 +43,35 @@ console.log(`res: ${JSON.stringify(res)}`);
 });
 }
 export default class App extends Component {
-componentDidMount() {
+ 
+//You can either create a function to get authentication on a button click or just add it in componentDidMount to fire it on loading. 
+//   goToFitbit() {
+//     OAuth(config.client_id, getData);
+//  } 
+// This can be used as a TouchableOpacity function and would be fired when user initiates the function.
+ 
+ 
+ componentDidMount() {
     OAuth(config.client_id, getData);
  }
+ // function would be fired when user once the rendering is complete.
  
  render() {
   return (
   <View style={styles.container}>
     <Text style={styles.welcome}>
-     Welcome to Fitbit Integration
+     Welcome to Fitbit Integration in React Native
     </Text>
+     <Text>
+     Note: Please check your console for values returned from the fitbit after successful redirect from the fitbit account website.
+    </Text>
+   
+   // Custom function to initate authentication
+   // Add this code only when you want it to be customised.
+    <TouchableOpacity onPress={()=>this.goToFitbit()}>
+     <Text>Go to Fitbit screen</Text>
+     </TouchableOpacity>
+  
   </View>
   );
  }
@@ -66,7 +86,7 @@ container: {
  backgroundColor: '#00a8b5',
 },
 welcome: {
- fontSize: 25,
+ fontSize: 16,
  textAlign: 'center',
  color: '#fff',
  margin: 10,
